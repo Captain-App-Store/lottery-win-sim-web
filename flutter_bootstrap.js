774baf6c0c66ae27c5818b203318ede6a37944f2
Thomas Cooper <tc@coope.biz>
2025-05-28 22:33:09 +0100

    Various Fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3346642328"
+    serviceWorkerVersion: "1199085630"
   }
 });
