d0e9a77018c40d8f923959f44a0624c3f23c92dd
Thomas Cooper <tc@coope.biz>
2025-06-05 20:40:19 +0100

    Update a few minor functionality tweaks

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "241be6e92ddb4fa55cf37d2a22fc6b01",
+const RESOURCES = {"flutter_bootstrap.js": "4d32e8a2805c9668f6687f230cd5f49a",
 "version.json": "ab4f790cf4de77edad1e569545dad814",
 "index.html": "43a1f67127089ed6b923f025ebedfbdd",
 "/": "43a1f67127089ed6b923f025ebedfbdd",
-"main.dart.js": "7c18c9eb7c6132e99b4e4dba7fa35a14",
+"main.dart.js": "38b68f2b0df2134d3895ab1ccd95e173",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "3437fe7ab0c4963f1afda3285bcde590",
 "icons/Icon-192.png": "5a108d0a8ead6d9a7ffeb399414bc4da",
